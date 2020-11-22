2fe4587394cf441a8b8d87d78f43d33e2f4e3734
Recep Öztürk <recep.fed@gmail.com>
2020-11-22 14:36:36 +0300

    Her gecenin sabahı, benim dermanım yok...

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import LeftPanel from "./LeftPanel";
 import send from "../assets/icons/send.png";
 import addicons from "../assets/icons/addicons.png";
+import p from "../assets/icons/p.png";
+import file from "../assets/icons/file.png";
+import more from "../assets/icons/more.png";
 import MessagesMiddleSection from "./MessagesMiddleSection";
 import { set } from "mongoose";
 const App = () => {
@@ -11,6 +14,13 @@ const App = () => {
       <div className="rightside">
         <MessagesMiddleSection />
         <div className="right-div">
+          <div className="top">
+            <img src={p} />
+            <p>Nika Jerrardo</p>
+            <p>last online 5 hours ago</p>
+            <img src={file} />
+            <img src={more} />
+          </div>
           <div className="input-area">
             <img src={addicons} />
             <hr />
